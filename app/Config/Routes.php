afe0e2aa27b2b4327b267b34f27efb2ed8848e6b
charllys castro <charllyscastro@gmail.com>
2024-04-29 22:54:12 -0300

    Revert "005 - gerando pdf com info bd filtradas no Dompdf"
    This reverts commit c90d421ca20983d2ba1ca75546131359e8a00eee.

--- app/Config/Routes.php
+++ app/Config/Routes.php
@@ -19,6 +19,3 @@ $routes->get('pdf-gerar', 'PdfController::pdf_gerar');
 $routes->get('pdf-gerar-imagem', 'PdfController::pdf_gerar_imagem');
 $routes->get('pdf-gerar-css-externo', 'PdfController::pdf_gerar_css_externo');
 $routes->get('pdf-gerar-relatorio-bd', 'PdfController::pdf_gerar_relatorio_bd');
-
-$routes->post('pdf-gerar-relatorio-filtro-bd', 'PdfController::pdf_gerar_relatorio_filtro_bd');
-
