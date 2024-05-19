d3ad181e153890f87c7eb5cc3cf087d667e87dca
charllys castro <charllyscastro@gmail.com>
2024-05-19 17:54:03 -0300

    Inserida opcoes de interacoes com poup

--- app/Views/Vis/index.php
+++ app/Views/Vis/index.php
@@ -25,66 +25,11 @@
     height: 600px;
     border: 1px solid lightgray;
   }
-
-  table.legend_table {
-    font-size: 11px;
-    border-width: 1px;
-    border-color: #d3d3d3;
-    border-style: solid;
+  .vis-tooltip{
+    max-width: 250px;
+    white-space: normal !important;
   }
 
-  table.legend_table,
-  td {
-    border-width: 1px;
-    border-color: #d3d3d3;
-    border-style: solid;
-    padding: 2px;
-  }
-
-  div.table_content {
-    width: 80px;
-    text-align: center;
-  }
-
-  div.table_description {
-    width: 100px;
-  }
-
-  #operation {
-    font-size: 28px;
-  }
-
-  #node-popUp {
-    display: none;
-    position: absolute;
-    top: 350px;
-    left: 170px;
-    z-index: 299;
-    width: 250px;
-    height: 120px;
-    background-color: #f9f9f9;
-    border-style: solid;
-    border-width: 3px;
-    border-color: #5394ed;
-    padding: 10px;
-    text-align: center;
-  }
-
-  #edge-popUp {
-    display: none;
-    position: absolute;
-    top: 350px;
-    left: 170px;
-    z-index: 299;
-    width: 250px;
-    height: 90px;
-    background-color: #f9f9f9;
-    border-style: solid;
-    border-width: 3px;
-    border-color: #5394ed;
-    padding: 10px;
-    text-align: center;
-  }
 </style>
 
 <body>
@@ -105,13 +50,15 @@
       id: <?php echo $pessoa->id ?>,
       label: "<?php echo $pessoa->nome ?>",
       image: "<?php echo site_url('img/user/' . $pessoa->imagem) ?>",
-      shape: "circularImage"
+      shape: "circularImage",
+      title: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum."
     },
     <?php foreach ($vinculos as $vinculo) : ?> {
         id: <?php echo $vinculo->pessoa_vinculo_id  ?>,
         label: "<?php echo $vinculo->vinculo->pessoa_vinculo_nome ?>",
         image: "<?php echo $vinculo->vinculo->pessoa_imagem ? site_url('img/user/' .  $vinculo->vinculo->pessoa_imagem) : site_url('img/user/sem_imagem.png') ?>",
-        shape: "circularImage"
+        shape: "circularImage",
+        title: "<?php echo $vinculo->vinculo->pessoa_vinculo_nome ?>"
       },
     <?php endforeach; ?>
 
@@ -119,7 +66,8 @@
         id: <?php echo $veiculo->id . hexdec(substr(md5($veiculo->placa), 0, 8));  ?>, //gera um valor em hexadecimal a partir do md5 da placa
         label: "<?php echo $veiculo->placa ?>",
         image: "<?php echo site_url('img/user/car.png') ?>",
-        shape: "circularImage"
+        shape: "circularImage",
+        title: "<?php echo $veiculo->placa ?>"
       },
     <?php endforeach; ?>
   ]);
@@ -129,7 +77,8 @@
     <?php foreach ($vinculos as $vinculo) : ?> {
         from: <?php echo $vinculo->pessoa_id ?>,
         to: <?php echo $vinculo->pessoa_vinculo_id ?>,
-        label: "<?php echo $vinculo->vinculo_nome ?>"
+        label: "<?php echo $vinculo->vinculo_nome ?>",
+       
       },
     <?php endforeach; ?>
 
@@ -148,7 +97,7 @@
     nodes: nodes,
     edges: edges
   };
-  
+
   var options = {
     manipulation: {
       enabled: true,
@@ -157,6 +106,9 @@
       deleteNode: true
 
     },
+    interaction: {
+      tooltipDelay: 300
+    }
   };
 
   var network = new vis.Network(container, data, options);
